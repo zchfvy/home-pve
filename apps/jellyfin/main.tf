ad3c8b35ddecd83844169b44b34c2ae242e34a18
Jason Hamilton-Smith <hs.jason@gmail.com>
2026-01-11 22:19:04 -0800

    Upgrade monitoring, increase jollyfin memory

--- apps/jellyfin/main.tf
+++ apps/jellyfin/main.tf
@@ -6,7 +6,7 @@ module "lxc_jellyfin" {
   proxmox_node_name    = var.proxmox_node_name
   disk_size            = 16
   cpu_cores            = 2
-  dedicated_memory     = 2048
+  dedicated_memory     = 4096
   ssh_public_key_file  = var.ssh_public_key_file
   ssh_private_key_file = var.ssh_private_key_file
 
